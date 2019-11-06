6e1b3d77ee31aac1d3eb3bc301d42a5f91a44cdb
Gaberu <33635431+gaberu13@users.noreply.github.com>
2019-11-06 21:47:58 +0100

    added list

--- src/main/java/mk/finki/ukim/mk/lab/Repository/PizzaRepository.java
+++ src/main/java/mk/finki/ukim/mk/lab/Repository/PizzaRepository.java
@@ -1,12 +1,15 @@
 package mk.finki.ukim.mk.lab.Repository;
 
 import mk.finki.ukim.mk.lab.Model.Pizza;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public  class PizzaRepository {
+    static List<Pizza> pizzas = new ArrayList<>(10);
     public List<Pizza> getAllPizzas() {
-        return null;
+        return pizzas;
     }
 
 }
--- src/main/java/mk/finki/ukim/mk/lab/Services/Impl/PizaaServiceImpl.java
+++ src/main/java/mk/finki/ukim/mk/lab/Services/Impl/PizaaServiceImpl.java
@@ -1,6 +1,7 @@
 package mk.finki.ukim.mk.lab.Services.Impl;
 
 import mk.finki.ukim.mk.lab.Model.Pizza;
+import mk.finki.ukim.mk.lab.Repository.PizzaRepository;
 import mk.finki.ukim.mk.lab.Services.PizzaService;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +9,15 @@ import java.util.List;
 
 @Service
 public class PizaaServiceImpl implements PizzaService {
+    private final PizzaRepository pizzaRepository;
+
+    public PizaaServiceImpl(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }
+
     @Override
     public List<Pizza> listPizzas() {
-        return null;
+        return this.pizzaRepository.getAllPizzas();
     }
 }
+
