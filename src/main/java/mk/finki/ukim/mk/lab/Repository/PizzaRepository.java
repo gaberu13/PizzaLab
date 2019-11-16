5a684686bc94cbcfeeb18f7e2204d493dcdfdc3a
Gaberu <33635431+gaberu13@users.noreply.github.com>
2019-11-16 15:04:42 +0100

    lab1 done

--- src/main/java/mk/finki/ukim/mk/lab/Repository/PizzaRepository.java
+++ src/main/java/mk/finki/ukim/mk/lab/Repository/PizzaRepository.java
@@ -7,7 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 @Component
 public  class PizzaRepository {
-    static List<Pizza> pizzas = new ArrayList<>(10);
+    private List<Pizza> pizzas = new ArrayList<>(10);
+    public PizzaRepository(){
+        this.pizzas.add(  new Pizza("Margherita ","tomato sauce, mozzarella"));
+        this.pizzas.add(   new Pizza("Carbonara ","fresh cream, mozzarella, bacon"));
+        this.pizzas.add(   new Pizza("Vegetariana ","tomato sauce, mushrooms"));
+        this.pizzas.add(   new Pizza("Vegetariana ","tomato sauce, mushrooms"));
+        this.pizzas.add(   new Pizza("Calzone ","Pizza dough, ricotta, pepperoni, pizza sauce, olive oil"));
+        this.pizzas.add(  new Pizza("Cheddar  ","cheddar, tomato sauce"));
+        this.pizzas.add(  new Pizza("Capricciosa  ","fresh cream, mozzarella, bacon"));
+    }
+
     public List<Pizza> getAllPizzas() {
         return pizzas;
     }
--- src/main/java/mk/finki/ukim/mk/lab/Web/ShowPizza.java
+++ src/main/java/mk/finki/ukim/mk/lab/Web/ShowPizza.java
@@ -2,11 +2,13 @@ package mk.finki.ukim.mk.lab.Web;
 
 import lombok.extern.slf4j.Slf4j;
 import mk.finki.ukim.mk.lab.Model.Pizza;
+import mk.finki.ukim.mk.lab.Repository.PizzaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,14 +18,9 @@ import java.util.List;
 public class ShowPizza {
   @GetMapping
     public String showPizzas(Model model){
-      List<Pizza> pizzas = Arrays.asList(
-              new Pizza("Margherita ","tomato sauce, mozzarella"),
-              new Pizza("Carbonara ","fresh cream, mozzarella, bacon"),
-              new Pizza("Vegetariana ","tomato sauce, mushrooms"),
-              new Pizza("Calzone ","Pizza dough, ricotta, pepperoni, pizza sauce, olive oil"),
-              new Pizza("Cheddar  ","cheddar, tomato sauce"),
-              new Pizza("Capricciosa  ","fresh cream, mozzarella, bacon")
-      );
+    List<Pizza> pizzas;
+    final PizzaRepository pizzaRepository = new PizzaRepository();
+    pizzas = pizzaRepository.getAllPizzas();
       model.addAttribute("pizzas",pizzas);
       return "listPizzas";
   }
